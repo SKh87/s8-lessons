3406dcb4a8a03b543893f463889f6bde77664f2b
skh <pepper8@yandex.ru>
2024-05-12 20:01:45 +0300

    SKh-s8-3-7-1

--- Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
+++ Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
@@ -1,10 +1,20 @@
 from pyspark.sql import SparkSession
 
-spark = (
-        #
-    )
+spark = (SparkSession.builder.appName("SKh-s8-3-7-1")
+         .config("spark.jars.packages", "org.postgresql:postgresql:42.4.0")
+         .getOrCreate()
+         )
 
-df = (spark.read
-        #
-        .load())
+df = (spark.read.format("jdbc")
+      .option("url", "jdbc:postgresql://rc1a-fswjkpli01zafgjm.mdb.yandexcloud.net:6432/de")
+      # .option("url", "rc1a-fswjkpli01zafgjm.mdb.yandexcloud.net")
+      # .option("port", 6432)
+      .option("user", "student")
+      .option("password", "de-student")
+      # .option("database", "de")
+      .option("schema", "public")
+      .option("dbtable", "marketing_companies")
+      .option("driver", "org.postgresql.Driver")
+      .load())
+df.printSchema()
 df.count()
