ce419d194704d8a474c4002ad088925c8b88baaf
skh <pepper8@yandex.ru>
2024-05-12 16:06:57 +0300

    SKh-s8-3-5-1

--- Тема 3. Настройка потока данных/5. Преобразование потоковых данных/Задание 1/realization.py
+++ Тема 3. Настройка потока данных/5. Преобразование потоковых данных/Задание 1/realization.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as f
+from pyspark.sql.functions import col, from_json, from_unixtime
 from pyspark.sql.types import StructType, StructField, DoubleType, StringType, TimestampType
 
 # необходимая библиотека с идентификатором в maven
 # вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
-kafka_lib_id =
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+kafka_lib_id = \
+    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
 
 # настройки security для кафки
 # вы можете использовать из с помощью метода .options(**kafka_security_options)
@@ -15,16 +16,43 @@ kafka_security_options = {
     'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";',
 }
 
+
 def spark_init() -> SparkSession:
-		pass
+    return (SparkSession.builder.
+            master("local").
+            appName("SKh-s8-3-5-1").
+            config("spark.jars.packages", kafka_lib_id).
+            getOrCreate())
 
 
 def load_df(spark: SparkSession) -> DataFrame:
-		pass # не забудьте здесь указать то-же имя топика что и в TOPIC_NAME 
+    return (spark.readStream.format("kafka")
+            .option('kafka.bootstrap.servers', 'rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091')
+            .option('kafka.security.protocol', 'SASL_SSL', )
+            .option('kafka.sasl.mechanism', 'SCRAM-SHA-512', )
+            .option('kafka.sasl.jaas.config',
+                    'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";', )
+            .option('subscribe', 'student.topic.cohort22.pepper8')
+            .load())
 
 
 def transform(df: DataFrame) -> DataFrame:
-    pass
+    incomming_message_schema = StructType([
+        StructField("client_id", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("lat", DoubleType(), True),
+        StructField("lon", DoubleType(), True),
+    ])
+    df.printSchema()
+    df = df.select(from_json(col("value").cast("string"), incomming_message_schema).alias("parsed_value"))
+    df.printSchema()
+    return df.select(
+        col("parsed_value.client_id"),
+        col("parsed_value.timestamp"),
+        col("parsed_value.lat"),
+        col("parsed_value.lon"),
+    )
+
 
 
 spark = spark_init()
